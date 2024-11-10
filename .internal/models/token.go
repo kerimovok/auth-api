feb61106b73a489e4dbb8104caca8a2cf2a18675
Orkhan Karimov <naxro.ok1@gmail.com>
2024-11-11 03:17:53 +0400

    refactor: naming consistency

--- .internal/models/token.go
+++ .internal/models/token.go
@@ -2,11 +2,10 @@ package models
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type Token struct {
+	ID        uint       `json:"id"`
 	UserID    uint       `gorm:"not null;index" json:"user_id"`
 	Token     string     `gorm:"not null;uniqueIndex" json:"token"`
 	Type      string     `gorm:"not null" json:"type"` // auth, email_verification, password_reset
@@ -14,8 +13,6 @@ type Token struct {
 	RevokedAt *time.Time `gorm:"default:null" json:"revoked_at,omitempty"`
 	UserAgent string     `gorm:"type:text" json:"user_agent"`
 	IP        string     `gorm:"type:varchar(45)" json:"ip"`
-	gorm.Model
-	ID        uint       `json:"id"`
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt time.Time  `json:"updated_at"`
 	DeletedAt *time.Time `json:"deleted_at,omitempty"`
--- .internal/models/user.go
+++ .internal/models/user.go
@@ -2,20 +2,17 @@ package models
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type User struct {
+	ID          uint       `json:"id"`
 	Email       string     `gorm:"uniqueIndex;not null" json:"email"`
 	Password    string     `gorm:"not null" json:"-"`
 	IsAdmin     bool       `gorm:"default:false" json:"is_admin"`
 	IsVerified  bool       `gorm:"default:false" json:"is_verified"`
 	IsBlocked   bool       `gorm:"default:false" json:"is_blocked"`
 	LastLoginAt *time.Time `gorm:"default:null" json:"last_login_at,omitempty"`
-	gorm.Model
-	ID        uint       `json:"id"`
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"updated_at"`
-	DeletedAt *time.Time `json:"deleted_at,omitempty"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
+	DeletedAt   *time.Time `json:"deleted_at,omitempty"`
 }
