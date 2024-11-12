398412a8be7cef81db0588db343ce52b41317bed
Orkhan Karimov <naxro.ok1@gmail.com>
2024-11-12 04:18:32 +0400

    refactor: db pass variable name

--- .internal/database/database.go
+++ .internal/database/database.go
@@ -18,7 +18,7 @@ func ConnectDB() error {
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_PASS"),
 		os.Getenv("DB_NAME"),
 		os.Getenv("DB_PORT"),
 	)
