1c5df68aa7462367cf5461a3b9989719e3dc4ebe
Orkhan Karimov <naxro.ok1@gmail.com>
2024-12-01 03:54:42 +0400

    feat: fiber middlewares

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
-	"auth-api/pkg/config"
-	"auth-api/pkg/database"
-	"auth-api/pkg/validator"
 	"log"
 	"os"
 
 	"auth-api/internal/routes"
+	"auth-api/pkg/config"
+	"auth-api/pkg/database"
+	"auth-api/pkg/validator"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/healthcheck"
+	"github.com/gofiber/fiber/v2/middleware/helmet"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 )
 
@@ -32,8 +40,26 @@ func init() {
 	}
 }
 
-func main() {
+func setupApp() *fiber.App {
 	app := fiber.New(fiber.Config{})
+
+	// Middleware
+	app.Use(helmet.New())
+	app.Use(cors.New())
+	app.Use(compress.New())
+	app.Use(healthcheck.New())
+	app.Use(requestid.New(requestid.Config{
+		Generator: func() string {
+			return uuid.New().String()
+		},
+	}))
+	app.Use(logger.New())
+
+	return app
+}
+
+func main() {
+	app := setupApp()
 
 	routes.SetupRoutes(app)
 
